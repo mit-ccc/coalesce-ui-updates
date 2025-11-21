859ce092728a3bac1ccc04bb70a07317cae804c5
jrmatt <jrmatt@media.mit.edu>
2025-11-21 10:45:24 -0500

    Revert "prevent cell detail expansion while dragging"
    This reverts commit 59c6d02f94109c4963ced48bbbafa963bafac61f.

--- ui/src/components/workspace_components/Cell.js
+++ ui/src/components/workspace_components/Cell.js
@@ -77,12 +77,7 @@ function Cell(props) {
     }
   };
 
-  // state variable to track whether cell is being dragged
-  const [isDragging, setIsDragging] = useState(false);
-
   const timerInterval = useRef();
-
-  const boxRef = useRef(null);
 
   // function to start or stop timer based on loadingCell
   useEffect(() => {
@@ -289,14 +284,10 @@ function Cell(props) {
   return (
     <ThemeProvider theme={theme}>
       <Box
-        tabIndex={0} 
-        ref={boxRef}  
         sx={{
           mt: 1,
           mb: 1
         }}
-        onFocusCapture={handleFocus}
-        onBlur={handleBlur}
       >
         {loadingCell === true ? (
           <Stack
@@ -339,14 +330,14 @@ function Cell(props) {
             <Stack 
               direction="column"
               spacing={0}
+              onClick={handleFocus}
               sx={{
                 padding: 3,
                 borderRadius: 3,
-                border: isActive && !isDragging ? "1.5px solid #e0e0e0" :  "1px dashed #e0e0e0",
-                boxShadow: isActive && !isDragging ? "2px 2px 6px rgba(0, 0, 0, 0.16)" : "none"
+                border: `1px dashed #e0e0e0`,
               }}
             >
-              { isActive && !isDragging && (
+              { isActive && (
               <Stack
                 direction="row"
                 alignItems="center"
@@ -467,7 +458,7 @@ function Cell(props) {
                   onBlur={handleEditGlobalMainText}
                 />
                 </Stack>
-                {cellInfo.cell_details.cell_type === "question" && isActive && !isDragging && (
+                {cellInfo.cell_details.cell_type === "question" && (
                   <Input
                     id={"cell-description-" + props.cell_id}
                     // defaultValue={cellInfo.cell_details.description}
@@ -495,13 +486,11 @@ function Cell(props) {
                 ]}
                 </Stack>
               </Stack>
-              { isActive && !isDragging && (
               <CellToolBar
                 cell_id={props.cell_id}
                 handleDeleteCell={props.handleDeleteCell}
                 setLoadingCell={setLoadingCell}
-              />
-              )}
+            />
             </Stack>
             <Tooltip             
               title={<Typography fontSize={14}>Move Cell</Typography>} 
@@ -515,16 +504,6 @@ function Cell(props) {
                 variant="secondary"
                 sx={{
                   marginLeft: '1rem'
-                }}
-                onMouseDown={() => {
-                  // blur Box if user is dragging cell
-                  setIsDragging(true);
-                  if (boxRef.current) {
-                    boxRef.current.blur();
-                  }
-                }}
-                onMouseUp={() => {
-                  setIsDragging(false);
                 }}
               >
                 <DragIndicatorIcon fontSize="medium" />
